8afe905b1d526ec9935f4296e30cd8e6ada5229f
Wade Spane <wadetheriver@gmail.com>
2018-03-09 10:32:00 -0800

    Variables

--- components/coffee/script.coffee
+++ components/coffee/script.coffee
@@ -1,2 +1,28 @@
+myList = ['Wade', 3, {dog1:"Ellie"}]
+
+myObject = {
+  myList: myList
+  invincible: "true"
+  name: "Wade"
+  Heroes: [
+        {
+          name: "Thor"
+          title: "God of Thunder"
+        }
+        {
+          name: "Loki"
+          title: "God of Mischief"
+        }
+  ]
+}
+
 do addStuff = ->
-  $('#content').append("Don't miss it!")
+  addALittle = "whoa Thor God of Thunder"
+  a=true
+  $('#content').append(addALittle)
+
+console.log myObject
+console.log JSON.stringify(myObject)
+console.log myObject.myList[2].dog1
+console.log myObject.Heroes[0].name + " is the " +
+  myObject.Heroes[0].title
--- components/scripts/script.js
+++ components/scripts/script.js
@@ -1,5 +1,40 @@
-var addStuff;
+var addStuff, myList, myObject;
+
+myList = [
+  'Wade',
+  3,
+  {
+    dog1: "Ellie"
+  }
+];
+
+myObject = {
+  myList: myList,
+  invincible: "true",
+  name: "Wade",
+  Heroes: [
+    {
+      name: "Thor",
+      title: "God of Thunder"
+    },
+    {
+      name: "Loki",
+      title: "God of Mischief"
+    }
+  ]
+};
 
 (addStuff = function() {
-  return $('#content').append("Don't miss it!");
+  var a, addALittle;
+  addALittle = "whoa Thor God of Thunder";
+  a = true;
+  return $('#content').append(addALittle);
 })();
+
+console.log(myObject);
+
+console.log(JSON.stringify(myObject));
+
+console.log(myObject.myList[2].dog1);
+
+console.log(myObject.Heroes[0].name + " is the " + myObject.Heroes[0].title);
