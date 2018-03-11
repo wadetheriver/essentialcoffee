80dd61c768a0bf09130c5f47dde263a296bb7cf0
Wade Spane <wadetheriver@gmail.com>
2018-03-10 21:30:06 -0800

    simple functions

--- components/coffee/script.coffee
+++ components/coffee/script.coffee
@@ -1,32 +1,7 @@
-names = [
-  "Barot Bellingham",
-  "Constance Smith",
-  "Hassum Harrod",
-  "Hillary Goldwynn",
-  "Xhou Ta"
-]
+myFunction = ->
+  console.log('hello')
 
-for name in names
-  console.log name
+myFunction()
 
-# get index and name from array
-for name, i in names
-  console.log "#{i} #{name}"
-
-
-for name, i in names when name isnt "Xhou Ta" or "Hillary Goldwynn"
-  console.log "#{i} #{name}"
-
-
-# Navigate an object
-
-dog = {
-  name: "Ellie"
-  collar: "Pink"
-  breed: "Redbone"
-  info: ->
-    console.log("#{this.name} #{this.collar} #{this.breed}")
-}
-
-for key, value of dog when key isnt info
-  console.log ("#{key}: #{value}")
+myFunction2 = (a,b) ->
+  console.log ('hi')
--- components/scripts/script.js
+++ components/scripts/script.js
@@ -1,38 +1,11 @@
-var dog, i, j, k, key, l, len, len1, len2, name, names, value;
+var myFunction, myFunction2;
 
-names = ["Barot Bellingham", "Constance Smith", "Hassum Harrod", "Hillary Goldwynn", "Xhou Ta"];
-
-for (j = 0, len = names.length; j < len; j++) {
-  name = names[j];
-  console.log(name);
-}
-
-// get index and name from array
-for (i = k = 0, len1 = names.length; k < len1; i = ++k) {
-  name = names[i];
-  console.log(`${i} ${name}`);
-}
-
-for (i = l = 0, len2 = names.length; l < len2; i = ++l) {
-  name = names[i];
-  if (name !== "Xhou Ta" || "Hillary Goldwynn") {
-    console.log(`${i} ${name}`);
-  }
-}
-
-// Navigate an object
-dog = {
-  name: "Ellie",
-  collar: "Pink",
-  breed: "Redbone",
-  info: function() {
-    return console.log(`${this.name} ${this.collar} ${this.breed}`);
-  }
+myFunction = function() {
+  return console.log('hello');
 };
 
-for (key in dog) {
-  value = dog[key];
-  if (key !== info) {
-    console.log(`${key}: ${value}`);
-  }
-}
+myFunction();
+
+myFunction2 = function(a, b) {
+  return console.log('hi');
+};
