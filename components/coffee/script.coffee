f4db4fc82e2f9cbcce1165dd5e87324530fa5d72
Wade Spane <wadetheriver@gmail.com>
2018-03-11 18:17:22 -0700

    switch statements

--- components/coffee/script.coffee
+++ components/coffee/script.coffee
@@ -1,30 +1,10 @@
-# info = [
-#   'Wade Spane',
-#   'http://somewebsite.com',
-#   'http://anotherwebsite.com',
-#   'http://thirdsite.com'
-# ]
-#
-# # splat
-# fillout = (name, links...) ->
-#   $('#content').append("#{name} </br>")
-#   for item in links
-#     $('#content').append("#{item} </br>")
-#
-# fillout(info...)
+append = (item) ->
+  $("#content").append "#{item}"
 
-info = [
-  'Ray Villalobos'
-  'Beaner',
-  'http://planetoftheweb.com'
-  'http://authoredcontent.com'
-  'http://iviewsource.com'
-]
+color = 'red'
 
-fillOut = (name, race, links...) ->
-  $('#content').append "#{name}<br>"
-  $('#content').append "#{race}<br>"
-  for item in links
-    $('#content').append "#{item}<br>"
-
-fillOut info...
+switch color
+  when 'pink' then append 'Ellie'
+  when 'green' then append 'Juno'
+  when 'red', 'orange' then append 'Mattie'
+  else append 'Unknown Dog'
--- components/scripts/script.js
+++ components/scripts/script.js
@@ -1,31 +1,22 @@
-// info = [
-//   'Wade Spane',
-//   'http://somewebsite.com',
-//   'http://anotherwebsite.com',
-//   'http://thirdsite.com'
-// ]
+var append, color;
 
-// # splat
-// fillout = (name, links...) ->
-//   $('#content').append("#{name} </br>")
-//   for item in links
-//     $('#content').append("#{item} </br>")
-
-// fillout(info...)
-var fillOut, info;
-
-info = ['Ray Villalobos', 'Beaner', 'http://planetoftheweb.com', 'http://authoredcontent.com', 'http://iviewsource.com'];
-
-fillOut = function(name, race, ...links) {
-  var i, item, len, results;
-  $('#content').append(`${name}<br>`);
-  $('#content').append(`${race}<br>`);
-  results = [];
-  for (i = 0, len = links.length; i < len; i++) {
-    item = links[i];
-    results.push($('#content').append(`${item}<br>`));
-  }
-  return results;
+append = function(item) {
+  return $("#content").append(`${item}`);
 };
 
-fillOut(...info);
+color = 'red';
+
+switch (color) {
+  case 'pink':
+    append('Ellie');
+    break;
+  case 'green':
+    append('Juno');
+    break;
+  case 'red':
+  case 'orange':
+    append('Mattie');
+    break;
+  default:
+    append('Unknown Dog');
+}
