6cbd4dc39a65a3ebce9147a75372197c889d5f91
Wade Spane <wadetheriver@gmail.com>
2018-03-15 10:14:57 -0700

    For loops, incrementing and decrementing

--- components/coffee/script.coffee
+++ components/coffee/script.coffee
@@ -30,3 +30,34 @@ dog = {
 
 for key, value of dog when key isnt "info"
   console.log ("#{key}: #{value}")
+
+#arguments array and iterating forward and back
+plus = () ->
+  console.log "plus v2"
+  sum = 0
+  `for (var i = arguments.length-1; i>=0; i--) {
+    sum += arguments[i]
+    }`
+  return sum
+
+console.log plus("bird",0,1,2,3,65)
+
+#  iterate forward
+plusv2 = () ->
+  console.log "plus v2"
+  sum=0
+  for argument in arguments by 1
+    sum += argument
+  return sum
+
+console.log plusv2(0,1,2,3,65,"bird")
+
+# iterate reverse as in original js
+plusv3 = () ->
+  console.log "plus v3"
+  sum=0
+  for argument in arguments by -1
+    sum += argument
+  return sum
+
+console.log plusv3(0,1,2,3,65,"bird")
--- components/scripts/script.js
+++ components/scripts/script.js
@@ -1,4 +1,4 @@
-var dog, i, j, k, key, l, len, len1, len2, name, names, value;
+var dog, i, j, k, key, l, len, len1, len2, name, names, plus, plusv2, plusv3, value;
 
 names = ["Barot Bellingham", "Constance Smith", "Hassum Harrod", "Hillary Goldwynn", "Xhou Ta"];
 
@@ -36,3 +36,44 @@ for (key in dog) {
     console.log(`${key}: ${value}`);
   }
 }
+
+//arguments array and iterating forward and back
+plus = function() {
+  var sum;
+  console.log("plus v2");
+  sum = 0;
+  for (var i = arguments.length-1; i>=0; i--) {
+    sum += arguments[i]
+    };
+  return sum;
+};
+
+console.log(plus("bird", 0, 1, 2, 3, 65));
+
+//  iterate forward
+plusv2 = function() {
+  var argument, len3, m, sum;
+  console.log("plus v2");
+  sum = 0;
+  for (m = 0, len3 = arguments.length; m < len3; m += 1) {
+    argument = arguments[m];
+    sum += argument;
+  }
+  return sum;
+};
+
+console.log(plusv2(0, 1, 2, 3, 65, "bird"));
+
+// iterate reverse as in original js
+plusv3 = function() {
+  var argument, m, sum;
+  console.log("plus v3");
+  sum = 0;
+  for (m = arguments.length - 1; m >= 0; m += -1) {
+    argument = arguments[m];
+    sum += argument;
+  }
+  return sum;
+};
+
+console.log(plusv3(0, 1, 2, 3, 65, "bird"));
